fefc522df2acd63cf0d76a268649788680ee53c2
CliffS <strohs1@gmail.com>
2020-08-21 22:30:42 -0400

    append-and-delete.rs fixed

--- rust-solutions/hackerrank/algorithms/src/bin/append-and-delete.rs
+++ rust-solutions/hackerrank/algorithms/src/bin/append-and-delete.rs
@@ -7,29 +7,38 @@
 /// convert `s` to `t` by performing exactly `k` of the above operations on `s`. If it's possible,
 /// print Yes. Otherwise, print No.
 
-
-fn append_and_delete(s: String, t: String, k: usize) {
-    // determine how many characters match from the beginning of s and t
-    let match_count = s.chars().zip(t.chars())
+fn matching_count(s: &str, t: &str) -> usize {
+    return s.chars().zip(t.chars())
         .take_while(|(sc, tc)| *sc == *tc)
         .count();
+}
 
-    let sb = s.len() - match_count;
-    let tb = t.len() - match_count;
+fn append_and_delete(s: &str, t: &str, k: usize) -> &'static str {
 
-    dbg!(match_count, sb, tb);
-
-    if (sb + tb) == k {
-        println!("Yes");
+    if k >= s.len() + t.len() {
+        "Yes"
     } else {
-        println!("No");
+        // see if any characters match
+        let count = matching_count(&s, &t);
+
+        if (k - (s.len() - count + t.len() - count)) % 2 == 0 {
+            "Yes"
+        } else {
+            "No"
+        }
     }
 }
 
 fn main() {
-    let s = String::from("hackerhappy");
-    let t = String::from("h");
-    let k: usize = 10;
 
-    append_and_delete(s, t, k);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::append_and_delete;
+
+    #[test]
+    fn test() {
+        assert_eq!(append_and_delete("hackerhappy", "hackerrank", 9), "Yes");
+    }
 }
