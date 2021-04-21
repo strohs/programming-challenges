d38df94ec0f8ce8d229c814e5a30dfa1da39cf26
cliff <strohs1@gmail.com>
2021-04-21 16:18:45 -0400

    reimplementation of phone number combinations in Rust

--- rust-solutions/leetcode/src/bin/phone-letter-combos.rs
+++ rust-solutions/leetcode/src/bin/phone-letter-combos.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-
-/// # Letter Combinations of a phone number
+/// # Leetcode 17 Letter Combinations of a phone number
 /// Given a string containing digits from `2-9` inclusive, return all possible letter combinations
 /// that the number could represent.
 /// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that
@@ -19,50 +17,75 @@ use std::collections::HashMap;
 /// input: "23"
 /// output: `["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]`
 ///
+/// ## Constraints
+/// `0 <= digits.length <= 4`
+/// `digits[i] is a digit in range ['2', '9']`
+///
+///
 /// FYI permutation - you care about the order of the elements
 ///     n! / (n - k)!
 /// combinations - you don't care about the order of the elements
 ///    combination formula, you have n objects and want to choose k    = n! / k!(n - k!)
 
-fn build_digit_map() -> HashMap<char, Vec<char>> {
-    let mut m = HashMap::with_capacity(8);
-    m.insert('2', vec!['a','b','c']);
-    m.insert('3', vec!['d','e','f']);
-    m.insert('4', vec!['g','h','i']);
-    m.insert('5', vec!['j','k','l']);
-    m.insert('6', vec!['m','n','o']);
-    m.insert('7', vec!['p','q','r','s']);
-    m.insert('8', vec!['t','u','v']);
-    m.insert('9', vec!['w','x','y','z']);
-    m
+
+pub fn letter_combinations(digits: String) -> Vec<String> {
+
+    const LETTERS: [&str; 26] =
+        [
+            "a", "b", "c",
+            "d", "e", "f",
+            "g", "h", "i",
+            "j", "k", "l",
+            "m", "n", "o",
+            "p", "q", "r", "s",
+            "t", "u", "v",
+            "w", "x", "y", "z"
+        ];
+
+    fn get_letters(digit: &str) -> &[&str] {
+        let ls = match digit {
+            "2" => &LETTERS[0..3],
+            "3" => &LETTERS[3..6],
+            "4" => &LETTERS[6..9],
+            "5" => &LETTERS[9..12],
+            "6" => &LETTERS[12..15],
+            "7" => &LETTERS[15..19],
+            "8" => &LETTERS[19..22],
+            "9" => &LETTERS[22..26],
+            _ => panic!("invalid digit {}", digit),
+        };
+        ls
+    }
+
+    // combine all strings in l1 with all string in l2, storing the combined string in l1
+    fn combine<T: AsRef<str>, U: AsRef<str>>(l1: &[T], l2: &[U]) -> Vec<String> {
+        let mut combos = vec![];
+        for l1s in l1 {
+            for l2s in l2 {
+                let mut combined_str = String::from(l1s.as_ref());
+                combined_str.push_str(l2s.as_ref());
+                combos.push(combined_str);
+            }
+        }
+        combos
+    }
+
+
+    if digits.is_empty() {
+        return vec![];
+    }
+
+    let mut combos = vec![String::from("")];
+    for digit in digits.split("").filter(|s| !s.is_empty()) {
+        let letters = get_letters(digit);
+        combos = combine(&combos, letters);
+    }
+
+    combos
 }
 
-/// combine each string in s1 with each char in s2
-fn combine_sets(s1: &Vec<String>, s2: &Vec<char>) -> Vec<String> {
-    let mut combis = vec![];
-    for s1 in s1.iter() {
-        for s2 in s2.iter() {
-            let mut s = String::new();
-            s.push_str(&s1.clone());
-            s.push(*s2);
-            combis.push(s);
-        }
-    }
-    combis
-}
 
 fn main() {
-    let digit_map = build_digit_map();
-    let input1 = "23";
-    let digits = input1.chars().collect::<Vec<char>>();
-
-    let init: Vec<String> = digit_map.get(&digits[0]).unwrap().iter().map(|&s| s.to_string()).collect();
-    let combis = digits.iter()
-        .skip(1)
-        .fold(init, |acc, digit| {
-            let digit_chars = digit_map.get(digit).unwrap();
-            combine_sets(&acc, digit_chars)
-    });
-
-    println!("{:#?}",&combis);
+    let combos = letter_combinations(String::from("234"));
+    dbg!(combos);
 }
