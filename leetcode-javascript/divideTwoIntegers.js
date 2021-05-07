12bf5356af39c539ff29e47d4bc5174055785e43
CliffS <strohs1@gmail.com>
2021-05-07 07:31:34 -0400

    re-impl of divide two integers in javascript

--- leetcode-javascript/divideTwoIntegers.js
+++ leetcode-javascript/divideTwoIntegers.js
@@ -18,34 +18,83 @@
 /// Output: 3
 /// Explanation: 10/3 = truncate(3.33333..) = 3.
 
-function divide(dividend, divisor) {
-  if (dividend === 0) return 0;
 
-  // compute the sign of the final quotient
-  let sign = -1;
-  if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
-    sign = 1;
+var divide = function (dividend, divisor) {
+
+  const MAX_POS_INT =  2147483647;
+  const MIN_NEG_INT = -2147483648;
+
+  let finalSign = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? -1 : 1;
+
+  // Check for overflow
+  if (divisor === 0 || (dividend === MIN_NEG_INT && divisor === -1)) {
+    return MAX_POS_INT;
+  }
+  if (divisor === dividend) {
+    return 1;
   }
 
+  let quot = 0;
   let dend = Math.abs(dividend);
   let dsor = Math.abs(divisor);
 
-  let quotient = 0;
   while (dend >= dsor) {
-    quotient++;
-    dend -= dsor;
+    let shift = 0;
+    let shiftedDivisor = dsor;
+    while (dend >= shiftedDivisor) {
+      shift += 1;
+      shiftedDivisor = dsor << shift;
+      // check for any overflow caused by JS numerics
+      if (shiftedDivisor < 0) {
+        break;
+      }
+    }
+    quot += (1 << (shift - 1));
+    dend -= dsor << (shift - 1);
   }
 
-  return quotient * sign;
-}
+  return finalSign === -1 ? -quot : quot;
+};
 
-const assert = require('assert');
-assert( divide(10,3) === 3);
 
-assert( divide(3,10) === 0);
+// function divide(dividend, divisor) {
+//   if (dividend === 0) return 0;
+//
+//   // compute the sign of the final quotient
+//   let sign = -1;
+//   if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) {
+//     sign = 1;
+//   }
+//
+//   let dend = Math.abs(dividend);
+//   let dsor = Math.abs(divisor);
+//
+//   let quotient = 0;
+//   while (dend >= dsor) {
+//     quotient++;
+//     dend -= dsor;
+//   }
+//
+//   return quotient * sign;
+// }
 
-assert( divide(-10,3) === -3);
+// const assert = require('assert');
+// assert(divide(10, 3) === 3);
+//
+// assert(divide(3, 10) === 0);
+//
+// assert(divide(-10, 3) === -3);
+//
+// assert(divide(-10, -3) === 3);
+//
+// assert(divide(0, 33) === 0);
+//
+// assert(divide(-2147483648, -1) === 2147483647);
 
-assert( divide(-10,-3) === 3);
-
-assert( divide(0,33) === 0);
+console.log(divide(10, 3));
+console.log(divide(0, 33));
+console.log(divide(-10, -3));
+console.log(divide(1, -1));
+console.log(divide(-3, 2));
+console.log(divide(-2147483648, -1)); // 2147483647
+console.log(divide(-2147483648, 1)); // -2147483648
